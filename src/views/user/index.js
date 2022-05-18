7864b22723f61a6fdc0e8439cfd6865c97f407d1
yuezhanhope <yuezhanhope@163.com>
2022-05-18 14:58:25 +0800

    新增唤起手机拨打电话发送邮件功能

--- src/views/user/index.js
+++ src/views/user/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import style from '../../assets/style/user.module.css'
-export default function User () {
+export default function User() {
 
   const [info, setinfo] = useState('')
   const [show, setshow] = useState(false)
@@ -8,7 +8,7 @@ export default function User () {
   const icons = [
     { id: 1, icon: 'iconfont icon-weixin', value: '微信：18329935900' },
     { id: 2, icon: 'iconfont icon-QQ', value: 'QQ：1114944681' },
-    { id: 3, icon: 'iconfont icon-youxiang', value: '邮箱：yuezhanhope@163.com' },
+    { id: 3, icon: 'iconfont icon-fapiaoyoujidizhi', value: '邮箱：yuezhanhope@163.com' },
     { id: 4, icon: 'iconfont icon-dianhua', value: '电话：18329935900' }
   ]
   const openInfo = (val) => {
@@ -19,6 +19,17 @@ export default function User () {
     }
     setinfo(val)
     setshow(true)
+  }
+  // 调用电话
+  const setPhone = () => {
+
+    if (info.substring(0, 2) === '电话') {
+      window.location.href = 'tel:18329935900#mp.weixin.qq.com'
+    }
+    if (info.substring(0, 2) === '邮箱') {
+      window.location.href = 'mailto:yuezhanhope@163.com'
+    }
+
   }
 
   return (
@@ -35,7 +46,7 @@ export default function User () {
           })}
 
         </ul>
-        {info === '' & !show ? false : <div className={style.info}>{info}</div>}
+        {info === '' & !show ? false : <div className={style.info} onClick={setPhone}>{info}</div>}
 
       </div>
 
